d856e2e273feaedb42578d92a07795cc0abd9c8e
Spencer Moody <spencer.moody@outlook.com>
2020-07-17 10:17:46 -0700

    Pass test for DiscountBreadCost

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,10 +1,31 @@
-namespace Calendar
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Model
 {
-  public class LeapYear
+  public static class Bread
   {
-    public bool IsLeapYear(int year)
+    // public int TotalBread { get; set; }
+    
+    // public Bread (int totalBread)
+    // {
+    //   TotalBread = totalBread;
+    // }
+
+    public static int AddBreadCost(int userInput)
     {
-      return year % 4 == 0;
+      int breadCost = 5;
+      int fullPrice = userInput * breadCost;
+      return fullPrice;
     }
+      
+
+    public static int DiscountBreadCost(int userInput)
+    { 
+      int breadCost = 5;
+      int discountCost = (userInput * breadCost) - ((userInput / 3)*breadCost);
+      return discountCost;
+    }
+
   }
 }
