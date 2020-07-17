94e6f456e139944136d464a4cddddb0eca442ffd
Spencer Moody <spencer.moody@outlook.com>
2020-07-17 13:43:47 -0700

    Add UI to Program.cs for menu option '1'

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,12 +1,28 @@
+using BakeryBread.Model;
+using BakeryPastry.Model;
 using System;
 
-namespace project
+namespace Program
 {
-  public class project
+  public class Program
   {
     public static void Main()
     {
-      
+
+      public static void MainMenu();
+      {
+        Console.WriteLine("Welcome to Pierres Bakery!");
+        Console.WriteLine("Would you like to order bread or pastries?");
+        Console.WriteLine("please type in '1' for bread or '2' for pastries.");
+        string menuOption = (Console.ReadLine());
+
+        if (menuOption = "1"){
+        }
+          Console.WriteLine("Todays specials! Buy 2, get 1 free. A single loaf costs $5.");
+          Console.WriteLine("How many loafs would you like to order?")
+          int breadOrder = int.Parse(Console.ReadLine());
+          
+      }  
     }
   }
 }
